4c70660ff345c1a7ee5e16cc08f656b723697c74
elijhonathan <eligrancej@hotmail.com>
2024-10-13 05:13:13 -0300

    ok

---
+++ Aula13/Exercicio01.js
@@ -0,0 +1,97 @@
+// Exercício 01
+console.log("Hello World!")
+
+
+// Exercício 02
+let numero1 = 2
+
+let numero2 = 5
+
+console.log(numero1 + numero2)
+
+// Exercício 03
+let validacao = 236
+
+
+if (validacao % 2 == 0 || validacao % 2 == 1)
+{
+    console.log("É um número")
+}
+
+else
+{
+    console.log("Não é um número")
+}
+
+// Exercício 04
+validacao = "te"
+
+
+if (validacao % 2 == 0 || validacao % 2 == 1)
+{
+    console.log("Não é uma String")
+}
+
+else
+{
+    console.log("É uma String")
+}
+
+// Exercicio 05
+validacao = true
+
+
+if (validacao == true || validacao == false)
+{
+    console.log("É Booleano")
+}
+
+else
+{
+    console.log("Não é Booleano")
+}
+
+// Exercício 06
+numero1 = 19
+
+numero2 = 7
+
+console.log(numero1 - numero2)
+
+// Exercicio 07
+numero1 = 2
+
+numero2 = 8
+
+console.log(numero1 * numero2)
+
+// Exercício 08
+numero1 = 16
+
+numero2 = 4
+
+console.log(numero1 / numero2)
+
+// Exercício 09
+validacao = 1028
+
+if (validacao % 2 == 0)
+{
+    console.log("É um número par")
+}
+else
+{
+    console.log("Não é um número par")
+}
+
+// Ecercício 10
+validacao = 1027
+
+if (validacao % 2 == 0)
+{
+    console.log("Não é um número ímpar")
+}
+else
+{
+    console.log("É um número ímpar")
+}
